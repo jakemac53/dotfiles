fb39b0c9cc51871655bb5bd073243270a4533d34
Jacob MacDonald <jakemac@google.com>
2018-02-22 12:25:56 -0800

    add optional version arg to update_sdk

--- update_sdk.sh
+++ update_sdk.sh
@@ -8,8 +8,14 @@ else
   PLATFORM="linux-x64"
 fi
 
+if [ -z "$1" ]; then
+  VERSION="latest"
+else
+  VERSION="$1"
+fi
+
 echo "Downloading latest dart"
 DOWNLOAD="https://storage.googleapis.com/dart-archive/channels/dev/release/"
-LATEST="latest/sdk/dartsdk-$PLATFORM-release.zip"
-curl $DOWNLOAD$LATEST -o /tmp/dart-release.zip
+VERSION_PATH="$VERSION/sdk/dartsdk-$PLATFORM-release.zip"
+curl $DOWNLOAD$VERSION_PATH -o /tmp/dart-release.zip
 unzip -qo /tmp/dart-release.zip -d ~
