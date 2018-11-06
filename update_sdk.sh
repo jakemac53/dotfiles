4c86e7edc8a31be4dae222185e18cea905e526f3
Jacob MacDonald <jakemac@google.com>
2018-11-06 07:17:26 -0800

    add channel support

--- update_sdk.sh
+++ update_sdk.sh
@@ -14,8 +14,14 @@ else
   VERSION="$1"
 fi
 
+if [ -z "$2" ]; then
+  CHANNEL="dev/release"
+else
+  CHANNEL="$2"
+fi
+
 echo "Downloading latest dart"
-DOWNLOAD="https://storage.googleapis.com/dart-archive/channels/dev/release/"
+DOWNLOAD="https://storage.googleapis.com/dart-archive/channels/$CHANNEL/"
 VERSION_PATH="$VERSION/sdk/dartsdk-$PLATFORM-release.zip"
 curl $DOWNLOAD$VERSION_PATH -o /tmp/dart-release.zip
 unzip -qo /tmp/dart-release.zip -d ~
